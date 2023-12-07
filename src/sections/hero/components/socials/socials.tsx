b840743c5df15368956b37b91f52eb4330b811c3
alterproger <alterproger@gmail.com>
2023-12-08 00:01:37 +0200

    feat: add socials component

---
+++ src/sections/hero/components/socials/socials.tsx
@@ -0,0 +1,37 @@
+import {
+  UilInstagram,
+  UilDribbble,
+  UilGithubAlt,
+} from '@iconscout/react-unicons';
+
+import styles from './styles.module.scss';
+
+const Socials = () => {
+  return (
+    <div className={styles['socials']}>
+      <a
+        className={styles['icon']}
+        href="https://www.instagram.com/"
+        target="_blank"
+      >
+        <UilInstagram />
+      </a>
+      <a
+        className={styles['icon']}
+        href="https://dribbble.com/"
+        target="_blank"
+      >
+        <UilDribbble />
+      </a>
+      <a
+        className={styles['icon']}
+        href="https://github.com/"
+        target="_blank"
+      >
+        <UilGithubAlt />
+      </a>
+    </div>
+  );
+};
+
+export { Socials };
