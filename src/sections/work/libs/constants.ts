c53768633c47f1e41e9433afa59239d413f1c7bd
alterproger <alterproger@gmail.com>
2023-12-13 14:05:08 +0200

    feat: add projects data

---
+++ src/sections/work/libs/constants.ts
@@ -0,0 +1,40 @@
+import work1 from '@/assets/img/works/work1.jpg';
+import work2 from '@/assets/img/works/work2.jpg';
+import work3 from '@/assets/img/works/work3.jpg';
+import work4 from '@/assets/img/works/work4.jpg';
+import work5 from '@/assets/img/works/work5.jpg';
+
+const PROJECTS = [
+  {
+    id: 1,
+    image: work1,
+    title: 'Web design',
+    category: 'web',
+  },
+  {
+    id: 2,
+    image: work2,
+    title: 'App movil',
+    category: 'app',
+  },
+  {
+    id: 3,
+    image: work3,
+    title: 'Brand design',
+    category: 'design',
+  },
+  {
+    id: 4,
+    image: work4,
+    title: 'App movil',
+    category: 'app',
+  },
+  {
+    id: 5,
+    image: work5,
+    title: 'Web design',
+    category: 'web',
+  },
+];
+
+export { PROJECTS };
