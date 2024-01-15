1d40c8bac17f2e31dacb3e1682f69c82d5010958
alterproger <pensilproggertopg@gmail.com>
2024-01-15 18:42:51 +0100

    Update Names

--- src/sections/hero/components/info/info.tsx
+++ src/sections/hero/components/info/info.tsx
@@ -1,23 +1,20 @@
-import HandIcon from '@/assets/icons/hand.svg';
-import SendIcon from '@/assets/icons/send.svg';
-import styles from './styles.module.scss';
+import HandIcon from "@/assets/icons/hand.svg";
+import SendIcon from "@/assets/icons/send.svg";
+import styles from "./styles.module.scss";
 
 const Info = () => {
   return (
-    <div className={styles['info']}>
-      <h1 className={styles['title']}>
-        John Tyshkevych
-        <HandIcon className={styles['hand']} />
+    <div className={styles["info"]}>
+      <h1 className={styles["title"]}>
+        Alex Tyshkevych
+        <HandIcon className={styles["hand"]} />
       </h1>
-      <h3 className={styles['subtitle']}>Software Engineer</h3>
-      <p className={styles['description']}>
+      <h3 className={styles["subtitle"]}>Software Engineer</h3>
+      <p className={styles["description"]}>
         I&apos;m creative designer based in New York, and I&apos;m very
         passionate and dedicated to my work.
       </p>
-      <a
-        className="button button--flex"
-        href="#contact"
-      >
+      <a className="button button--flex" href="#contact">
         Say hello
         <SendIcon className="button-icon" />
       </a>
