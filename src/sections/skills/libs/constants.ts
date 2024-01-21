d0d545a1ca064967a7fa59ceede1e2a2dd49ef51
alterproger <pensilproggertopg@gmail.com>
2024-01-21 11:24:28 +0100

    Update skill block

--- src/sections/skills/libs/constants.ts
+++ src/sections/skills/libs/constants.ts
@@ -1,54 +1,45 @@
 const FRONTEND_SKILLS = [
   [
     {
-      name: 'React.js / Next.js',
+      name: "React.js / Next.js",
     },
     {
-      name: 'Javascript (ES6+) / Typescript',
+      name: "Javascript (ES6+) / Typescript",
     },
     {
-      name: 'HTML5 / CSS3 / SASS/ SCSS / Tailwind',
+      name: "HTML5 / CSS3 / SASS/ SCSS / Tailwind / Ant Design/ MUI",
     },
     {
-      name: 'Functional components/ class components / hooks / context API',
+      name: "Functional components/ class components / hooks / context API",
     },
     {
-      name: 'Redux / Redux-thunk / Redux Toolkit',
+      name: "Redux / MobX / Redux Toolkit / Recoil / Zustand",
     },
     {
-      name: 'React-hook-form / Formik',
+      name: "React-hook-form / Formik",
     },
     {
-      name: 'MobX / GraphQL / Recoil',
-    },
-    {
-      name: 'Antd design/ Material UI',
+      name: "GraphQL / Apollo / WebSockets",
     },
   ],
   [
     {
-      name: 'GitHub / GitLab / Bitbucket',
+      name: "Jira / Confluence/ Agile / Trello",
     },
     {
-      name: 'Jira / Confluence/ Agile / Trello',
+      name: "Unit Testing (Jest, Mocha, Jasmine)",
     },
     {
-      name: 'Unit Testing (Jest, Mocha, Jasmine)',
+      name: "End-to-End Testing (Cypress, Selenium)",
     },
     {
-      name: 'Integration Testing',
+      name: "i18n / l10n",
     },
     {
-      name: 'End-to-End Testing (Cypress, Selenium)',
+      name: "WebSockets",
     },
     {
-      name: 'i18n / l10n',
-    },
-    {
-      name: 'WebSockets',
-    },
-    {
-      name: 'SEO optimization / Google Analytics',
+      name: "SEO optimization / Google Analytics",
     },
   ],
 ];
@@ -56,69 +47,69 @@ const FRONTEND_SKILLS = [
 const BACKEND_SKILLS = [
   [
     {
-      name: 'Node.js',
+      name: "Node.js",
     },
     {
-      name: 'Python',
+      name: "Python",
     },
     {
-      name: 'Express / Nest / Koa',
+      name: "Express / Nest / Koa",
     },
     {
-      name: 'GraphQl',
+      name: "GraphQl",
     },
     {
-      name: 'Sequalize / TypeORM / Mongoose',
+      name: "Sequalize / TypeORM / Mongoose",
     },
     {
-      name: 'Jest / Mocha',
+      name: "Jest / Mocha",
     },
     {
-      name: 'OAuth',
+      name: "OAuth",
     },
     {
-      name: 'JWT',
+      name: "JWT",
     },
     {
-      name: 'Swagger',
+      name: "Swagger",
     },
     {
-      name: 'API Blueprint',
+      name: "API Blueprint",
     },
   ],
   [
     {
-      name: 'MySQL',
+      name: "MySQL",
     },
     {
-      name: 'Firebase',
+      name: "Firebase",
     },
     {
-      name: 'SQL',
+      name: "SQL",
     },
     {
-      name: 'PostgresQL',
+      name: "PostgresQL",
     },
     {
-      name: 'Oracle',
+      name: "Oracle",
     },
     {
-      name: 'Redis',
+      name: "Redis",
     },
     {
-      name: 'Docker',
+      name: "Docker",
     },
     {
-      name: 'AWS Lambda',
+      name: "AWS Lambda",
     },
     {
-      name: 'WebSocket',
+      name: "WebSocket",
     },
     {
-      name: 'Cron',
+      name: "Cron",
     },
     {
-      name: 'Nginx',
+      name: "Nginx",
     },
   ],
 ];
